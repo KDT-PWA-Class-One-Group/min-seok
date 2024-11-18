6430347a69e1514127c28855cd2350295f1bd2a7
pukkok <krystalgggg@gmail.com>
2024-11-18 16:54:15 +0900

    색상을 넣으면 배경색이 변하도록 구현

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ console.log('자바스크립트 동작')
 
 const filterForm = document.getElementById('filter')
 const submitBtn = filterForm.querySelector('button')
-
+const card = document.getElementById('card')
 submitBtn.addEventListener('click', submitEvent)
 
 function submitEvent (e) {
+    const value = filterForm.querySelector('input').value
     e.preventDefault()
+    if(value === '') card.style.backgroundColor = ''
+    card.style.backgroundColor = value
 }

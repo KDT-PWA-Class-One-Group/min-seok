c63516f6117b7afc0e3dfb1999a46efcb36a4080
pukkok <krystalgggg@gmail.com>
2024-11-18 16:39:29 +0900

    제출 버튼 js 연결

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 console.log('자바스크립트 동작')
+
+const filterForm = document.getElementById('filter')
+const submitBtn = filterForm.querySelector('button')
+
+submitBtn.addEventListener('click', submitEvent)
+
+function submitEvent (e) {
+    e.preventDefault()
+}
